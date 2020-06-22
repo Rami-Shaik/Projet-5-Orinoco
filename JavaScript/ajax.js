9232561cec49dc3ff0e6002a1d22ba31f35d3354
Rami <shaiknaif.rami@gmail.com>
2020-06-22 16:01:38 +0200

    finalisation premiere page

--- JavaScript/ajax.js
+++ JavaScript/ajax.js
@@ -1,48 +1,21 @@
-// Exécute un appel AJAX GET
-// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
-function ajaxGet(url, callback) {
-    let req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            // Appelle la fonction callback en lui passant la réponse de la requête
-            callback(req.responseText);
+const getTeddies = async function () {
+    try {
+        let response = await fetch('http://localhost:3000/api/teddies')
+        if (response.ok) {
+            let data = await response.json()
+            console.log(data)
         } else {
-            console.error(req.status + " " + req.statusText + " " + url);
+            console.error('Retour du serveur : ', response.status)
         }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send(null);
-}
-
-function afficher(reponse) {
-    console.log(reponse);
-}
-
-ajaxGet("http://localhost:3000/api/teddies", afficher);
+    } catch (e) {
+        console.log(e)
+    }   
+}  
 
 
 
-let articlesElt = document.getElementById("peluches");
-ajaxGet("http://localhost:3000/api/teddies", function (reponse) {
-    // Transforme la réponse en un tableau d'articles
-    let articles = JSON.parse(reponse);
-    articles.forEach(function (article) {
-        // Ajout du titre et du contenu de chaque article
-        let nomElt = document.createElement("h2");
-        nomElt.textContent = article.name;
-        let prixElt = document.createElement("h3");
-        prixElt.textContent = article.price;
-        articlesElt.appendChild(nomElt);
-        articlesElt.appendChild(prixElt);
-    });
-});
 
-document.getElementById('peluches').innerHTML += '<div id= "premier" class="article"></div>';
-document.getElementById('peluches').innerHTML += '<div id= "deuxieme" class="article"></div>';
-document.getElementById('peluches').innerHTML += '<div id= "troisieme" class="article"></div>';
-document.getElementById('peluches').innerHTML += '<div id= "quatrieme" class="article"></div>';
-document.getElementById('peluches').innerHTML += '<div id= "cinquieme" class="article"></div>';
+
+
+
 
--- JavaScript/server.js
+++ JavaScript/server.js
@@ -1,22 +1,62 @@
-const url = "http://localhost:3000/api/teddies";
+let teddy = function (url) {
+    return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+  
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseText);
+          } else {
+            reject(xhr);
+            alert("Nous sommes désolés, le serveur ne répond pas ! ")
+          };
+        };
+      };
+      xhr.open('GET','http://localhost:3000/api/teddies/', true);
+      xhr.send();
+    });
+  };
 
-let requete = new XMLHttpRequest();
-requete.open('GET', url);
-requete.responseType = "json";
-requete.send();
 
-requete.onload = function () {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-        if (requete.status === 200) {
-            let reponse = requete.response;
-            console.log(reponse);
-        }
-    }
+let catchError = function(e){
+  console.error('Erreur AJAX', e);
 };
 
 
-document.getElementById("listes").innerHTML += '<div id="premier" class="articles"></div>';
-document.getElementById("listes").innerHTML += '<div id="deuxieme" class="articles"></div>';
-document.getElementById("listes").innerHTML += '<div id="troisieme" class="articles"></div>';
-document.getElementById("listes").innerHTML += '<div id="quatrieme" class="articles"></div>';
-document.getElementById("listes").innerHTML += '<div id="cinquieme" class="articles"></div>';
+let products = function () {
+  return teddy('http://localhost:3000/api/teddies/').then(function (response) {
+    let products = JSON.parse(response);
+    return products;
+  });
+};
+
+
+let oursElt = document.getElementById('peluches');
+
+
+products().then(function(products){
+console.log(products);
+products.forEach( teddy=> {
+    let divElt = document.createElement('div');
+    divElt.className= "module-border-wrap";
+    let divDeuxElt = document.createElement('div');
+    divDeuxElt.className= "module";
+    let imageElt = document.createElement('img');
+    imageElt.src =  teddy.imageUrl;
+    let nomElt = document.createElement('h2');
+    nomElt.textContent = teddy.name;
+    let priceElt = document.createElement('h3');
+    priceElt.textContent = ((teddy.price) /100) + ' €';
+    let idElt = teddy._id;
+    let lienElt = document.createElement('a');
+    lienElt.className = "lienProduit";
+    lienElt.href = 'produit.html?id=' + teddy._id;
+    oursElt.appendChild(divElt);
+    divElt.appendChild(divDeuxElt);
+    divDeuxElt.appendChild(lienElt);
+    lienElt.appendChild(nomElt);
+    lienElt.appendChild(imageElt);
+    lienElt.appendChild(priceElt);
+  });
+});
+
